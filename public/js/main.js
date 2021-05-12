5dea22e956fe2b4853aacb372d0be824215d4104
Antonia Pettersson <antoniapettersson@iMac-som-tillhor-Antonia.local>
2021-05-12 10:46:27 +0200

    utils folder

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,21 @@
 // access the form
 const chatForm = document.getElementById("chat-form");
-
+const chatMessages = document.querySelector(".chat-messages");
 const socket = io();
+
+// Get userName and room from URL
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+console.log(username, room);
 
 //Message from server
 socket.on("message", (message) => {
   console.log(message);
   outputMessage(message);
+
+  //scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 // Message submit
@@ -18,15 +27,19 @@ chatForm.addEventListener("submit", (e) => {
 
   //Emit massage to the server
   socket.emit("chatMassage", msg);
+
+  //Clear input
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
 });
 
 // outoutMessage to DOM
 function outputMessage(message) {
   const div = document.createElement("div");
   div.classList.add("message");
-  div.innerHTML = `<p class="meta">Brad <span>9:20pm</span></p>
+  div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
     <p class="text">
-     ${message}
+     ${message.text}
     </p>`;
 
   document.querySelector(".chat-messages").appendChild(div);
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
 
 const app = express();
 const server = http.createServer(app);
@@ -10,25 +11,30 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+const botName = "ChatCord Bot";
+
 // run when a clioent connects
 io.on("connection", (socket) => {
   // welcome , to the current client
-  socket.emit("message", "welcome to chattcord");
+  socket.emit("message", formatMessage(botName, "Welcome to ChatCord!"));
 
   // to all the clients, exept the client that connecting !Broadcast when a user connects
-  socket.broadcast.emit("message", "A user has joined the chat");
+  socket.broadcast.emit(
+    "message",
+    formatMessage(botName, "A user has joined the chat")
+  );
 
   // To All clients
   //io.emit()
 
   // runs when client disconnects
   socket.on("disconnect", () => {
-    io.emit("message", "A user has left the chat");
+    io.emit("message", formatMessage(botName, "A user has left the chat"));
   });
 
   // Listen for chatMassage
   socket.on("chatMassage", (msg) => {
-    io.emit("message", msg);
+    io.emit("message", formatMessage("User", msg));
   });
 });
 
